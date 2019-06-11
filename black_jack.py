a282f039bc11598ce573727425896d8ee73bc715
Apekshit <apekshit33@gmail.com>
2019-06-12 04:03:35 +0530

    Added class for Hand delt

--- black_jack.py
+++ black_jack.py
@@ -3,52 +3,66 @@ Text based Black Jack game
 '''
 
 #To shuful the deck
-
 import random
 
 
 #Global variables to create the deck of cards
-
-suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
-ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
+SUITS = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+RANKS = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen',
+         'King', 'Ace')
+VALUES = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9,
+          'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
 
 
 #Boolean to control the flow of game
-
-playing = True
+PLAYING = True
 
 
 class Card():
     """Class for card object"""
-    
+
     def __init__(self, suit, rank):
         self.suit = suit
         self.rank = rank
 
     def __str__(self):
         '''Method to print a Card'''
-        return self.rank+" of "+self.suit
+
+        return self.rank + " of " + self.suit
 
 
 class Deck():
     """Store 52 card objects in a list (deck) that can later be shuffled"""
-    
+
     def __init__(self):
-        self.deck = [] # Star with an empty list
-        for suit in suits:
-            for rank in ranks:
+        self.deck = [] # Start with an empty list
+        for suit in SUITS:
+            for rank in RANKS:
                 self.deck.append(Card(suit, rank))
 
     def __str__(self):
         '''Method to print the deck. May only be used for debugging'''
+
         deck_comp = ''
         for card in self.deck:
             deck_comp += '\n'+card.__str__()
+        return 'The deck has:' + deck_comp
 
     def shuffle(self):
+        '''Method to shuffle the deck'''
+
         random.shuffle(self.deck)
 
     def deal(self):
+        '''Method to deal the card'''
+
         single_card = self.deck.pop()
         return single_card
+
+class Hand():
+    """Class for Hand that has been delt"""
+
+    def __init__(self):
+        self.cards = [] # Hand starts empty
+        self.value = 0  # Start value is 0
+        self.aces = 0   # Attribute to keep track of aces
